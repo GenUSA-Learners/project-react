0db34641b9689256b2d230b0396902fe05db4949
Jon Jackson <ocskier@gmail.com>
2023-06-15 21:06:29 -0400

    Adds semantic ui react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Button } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>React App</h1>
+      <div>
+        <Button loading primary>
+          Loading
+        </Button>
+      </div>
     </div>
   );
 }
