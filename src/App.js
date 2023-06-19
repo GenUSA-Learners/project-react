4a2659f7546422d197d10b7d5b5a0b85db01aa81
Jon Jackson <ocskier@gmail.com>
2023-06-18 21:16:38 -0400

    Adds header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import { Button } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
-import './App.css';
+import { Header } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
-      <div>
-        <Button loading primary>
-          Loading
-        </Button>
-      </div>
+      <Header className="app-header" size="huge">
+        Task Manager
+      </Header>
     </div>
   );
 }
 
 export default App;
+
