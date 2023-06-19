e4d229a0347614f97e801143ddc565fcdfc19a06
Jon Jackson <ocskier@gmail.com>
2023-06-18 22:34:12 -0400

    Adds a datepicker

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,51 +1,74 @@
-import { Advertisement, Button, Form, Input, Select } from "semantic-ui-react";
+import { useState } from "react";
+import {
+  Advertisement,
+  Button,
+  Form,
+  Input,
+  Message,
+  // Select,
+  TextArea,
+} from "semantic-ui-react";
+import DatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
 import "./TaskForm.css";
 
-const TaskForm = () => (
-  <>
-    <Advertisement
-      className="form-header"
-      unit="large leaderboard"
-      test="Add a Task"
-    />
-    <Form className="form" error>
-      <Form.Group widths="equal">
-        <Form.Field
-          id="form-input-control-first-name"
-          control={Input}
-          label="First name"
-          placeholder="First name"
-        />
-        <Form.Field
-          id="form-input-control-last-name"
-          control={Input}
-          label="Last name"
-          placeholder="Last name"
-        />
-        {/* <Form.Field
-          control={Select}
-          options={genderOptions}
-          label={{ children: "Gender", htmlFor: "form-select-control-gender" }}
-          placeholder="Gender"
-          search
-          searchInput={{ id: "form-select-control-gender" }}
-        /> */}
-      </Form.Group>
-      <Form.Field
-        id="form-input-control-error-email"
-        control={Input}
-        label="Email"
-        placeholder="joe@schmoe.com"
-        error={{
-          content: "Please enter a valid email address",
-          pointing: "below",
-        }}
+const TaskForm = () => {
+  const [dueDate, setDueDate] = useState(null);
+  const onChange = (date) => setDueDate(date);
+
+  return (
+    <>
+      <Advertisement
+        className="form-header"
+        unit="large leaderboard"
+        test="Add a Task"
       />
-      {/* Datepicker */}
-      <Form.Field id="submit-button" control={Button} content="Submit" />
-    </Form>
-  </>
-);
+      <Form className="task-form" error loading={false} success>
+        <Form.Field
+          id="form-input-control-name"
+          control={Input}
+          label="Task Name"
+          placeholder="What is the name of the task"
+          required
+          error={{
+            content: "Please enter a valid email address",
+            pointing: "below",
+          }}
+        />
+        <Form.Field
+          id="form-input-control-description"
+          control={TextArea}
+          label="Task Description"
+          placeholder="Add a description"
+        />
+        <Form.Group className="form-group" widths="equal">
+          <Form.Field
+            id="form-input-control-error-assigned"
+            control={Input}
+            label="Assigned To"
+            placeholder="joe@schmoe.com"
+            required
+            error={{
+              content: "Please enter a valid email address",
+              pointing: "below",
+            }}
+          />
+          <DatePicker
+            className="form-datepicker"
+            onChange={onChange}
+            placeholderText={dueDate || "Due Date"}
+          />
+        </Form.Group>
+        <Form.Field id="Add Task" control={Button} content="Submit" />
+        <Message
+          success
+          header="Form Completed"
+          content="You're all signed up for the newsletter"
+        />
+      </Form>
+    </>
+  );
+};
 
 export default TaskForm;
