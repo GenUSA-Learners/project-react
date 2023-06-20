1497077df362ca7666e1efca512a2d103116c74d
Jon Jackson <ocskier@gmail.com>
2023-06-20 10:16:11 -0400

    Adds basic structure for task card

---
+++ src/components/TaskCard/TaskCard.jsx
@@ -0,0 +1,12 @@
+import { useCallback, useEffect, useState } from 'react';
+import {} from 'semantic-ui-react';
+
+import './TaskCard.css';
+
+const TaskCard = () => {
+  const handleClick = useCallback(() => {}, []);
+
+  return <></>;
+};
+
+export default TaskCard;
